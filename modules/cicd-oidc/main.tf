aa1c3a3fe94ec5e0ed11fee7c645569fa6255b31
mannix.gr <mannixg.is@gmail.com>
2026-09-17 14:09:47 -0400

    updated the README.MD.

--- modules/cicd-oidc/main.tf
+++ modules/cicd-oidc/main.tf
@@ -40,12 +40,17 @@ resource "aws_iam_role_policy"  "terraform_state" {
         Version ="2012-10-17"
         Statement = [{
             Effect = "Allow"
-            Action = ["s3:Getobject", "s3:PutObject", "s3:ListBucket"]
+            Action = ["s3:GetObject", "s3:PutObject", "s3:DeleteObject", "s3:ListBucket"]
             Resource = [
                 "arn:aws:s3:::mannix-enterprise-hybrid-aws-tfstate",
                 "arn:aws:s3:::mannix-enterprise-hybrid-aws-tfstate/*"
 
             ]
+        },
+        {
+            Effect = "Allow"
+            Action = ["ec2:DescribeAddressesAttribute"]
+            Resource = "*"
         }]
     })
 }
